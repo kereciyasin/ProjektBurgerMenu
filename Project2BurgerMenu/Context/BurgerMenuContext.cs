08b1f182f61ea8218812bdefa3c1193d8af734d4
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-08 22:45:21 +0200

    context added

---
+++ Project2BurgerMenu/Context/BurgerMenuContext.cs
@@ -0,0 +1,25 @@
+﻿using Project2BurgerMenu.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Project2BurgerMenu.Context
+{
+    public class BurgerMenuContext: DbContext
+    {
+        /* Category c# kullanacagimiz sinifin ismi
+         * Categories ise veritabanindaki tablonun ismi
+         */
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<DealOfTheDay> DealOfTheDays { get; set; }
+
+
+    }
+    
+    
+}
