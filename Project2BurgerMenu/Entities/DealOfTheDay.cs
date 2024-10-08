2cd47cc236645163e778e6e41e90dd07027de28e
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-08 22:24:03 +0200

    DealOfTheDay added

---
+++ Project2BurgerMenu/Entities/DealOfTheDay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project2BurgerMenu.Entities
+{
+    public class DealOfTheDay
+    {
+        public int DealOfTheDayId { get; set; }
+        public string Title { get; set; }
+        public string DealOfTheDayDescription { get; set; }
+        public decimal DealOfTheDayPrice { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Project2BurgerMenu/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project2BurgerMenu.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [StringLength(100)]
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
