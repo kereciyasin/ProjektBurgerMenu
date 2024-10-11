b2fa1e44e59bbc6222108d1ba0b9ca9223e0bcca
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-11 08:34:01 +0200

    Product Controller added

---
+++ Project2BurgerMenu/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project2BurgerMenu.Context;
+
+namespace Project2BurgerMenu.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        BurgerMenuContext db = new BurgerMenuContext(); 
+        public ActionResult ProductList()
+        {
+            var productList = db.Products.ToList();
+            return View(productList);
+        }
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(Entities.Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+    }
+}
