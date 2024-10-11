9319a500c9349aaf4bd54c4ca6ba252140c41b49
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-11 10:18:14 +0200

    login added

--- Project2BurgerMenu/Controllers/LoginController.cs
+++ Project2BurgerMenu/Controllers/LoginController.cs
@@ -1,17 +1,42 @@
-﻿using System;
+﻿using Project2BurgerMenu.Context;
+using Project2BurgerMenu.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Project2BurgerMenu.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        BurgerMenuContext db = new BurgerMenuContext();
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin admin)
+        {
+            /* Admin tablosundaki sartlari karsiliyorsa  */
+            var values = db.Admins.FirstOrDefault(x => x.Username == admin.Username && x.Password == admin.Password);
+            if (values != null)
+            {
+                /* Tarayiciya kayit edilmesin diyorsak false, kayit edilsin diyorsak true */
+                FormsAuthentication.SetAuthCookie(values.Username, false);
+                Session["Username"] = values.Username.ToString();
+                /* Index acilsin product kontrollardaki buda admin icinde */
+                return RedirectToAction("Index", "Product", "Admin");
+
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Kullanıcı adı veya şifre hatal��!";
+                return View();
+            }
+
         }
     }
 }
