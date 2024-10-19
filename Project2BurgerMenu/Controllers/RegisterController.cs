88218af75a51e87ec4f59d44afe08c208c34941c
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-19 22:54:17 +0200

    register controller added

---
+++ Project2BurgerMenu/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project2BurgerMenu.Context;
+using Project2BurgerMenu.Entities;
+
+namespace Project2BurgerMenu.Controllers
+{
+    public class RegisterController : Controller
+    {
+        BurgerMenuContext db = new BurgerMenuContext(); 
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin admin)
+        {
+            db.Admins.Add(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Login");
+        }   
+
+    }
+}
