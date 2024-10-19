19dc7036161243933bbd2e4cfc8f3f2e753ccbcf
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-20 00:41:04 +0200

    partial head added

---
+++ Project2BurgerMenu/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2BurgerMenu.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+
+            return PartialView();   
+        }
+
+        public Par
+    }
+}
