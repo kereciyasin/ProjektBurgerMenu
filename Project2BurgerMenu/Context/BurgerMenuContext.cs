42018cde30feecd5a6e4f6edf0632aab75e1fc77
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-20 15:30:47 +0200

    partialreservation added

--- Project2BurgerMenu/Context/BurgerMenuContext.cs
+++ Project2BurgerMenu/Context/BurgerMenuContext.cs
@@ -17,7 +17,8 @@ namespace Project2BurgerMenu.Context
         public DbSet<Product> Products { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<DealOfTheDay> DealOfTheDays { get; set; }
-        public DbSet<Admin> Admins { get; set; }    
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
 
     }
     
--- Project2BurgerMenu/Controllers/DefaultController.cs
+++ Project2BurgerMenu/Controllers/DefaultController.cs
@@ -37,8 +37,9 @@ namespace Project2BurgerMenu.Controllers
             return PartialView(deal);
         }
         public PartialViewResult PartialMenu()
-        {     
-            return PartialView();
+        {   
+            var values = db.Products.ToList();
+            return PartialView(values);
         }
         public PartialViewResult PartialCategory() 
         {
@@ -55,6 +56,24 @@ namespace Project2BurgerMenu.Controllers
         }
         public PartialViewResult PartialScripts() {
             return PartialView();
-        }   
+        }
+
+        [HttpGet]
+        public PartialViewResult PartialReservation()
+        {
+            return PartialView();
+        }
+
+
+        [HttpPost]
+        public PartialViewResult PartialReservation(Reservation reservation)
+        {
+            reservation.ReservationStatus = "Beklemede";
+            reservation.PeopleCount = 0;
+            reservation.ReservationDate = DateTime.Now;    
+            db.Reservations.Add(reservation);
+            db.SaveChanges();
+            return PartialView();
+        }
     }
 }
